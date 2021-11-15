abff69746403488b9776109a5bbfb07079558a9a
Liz Rodrigues <liz.oriana14@gmail.com>
2021-11-15 15:40:50 -0500

    correciones migrations

--- database/migrations/2021_11_16_180748_create_items_table.php
+++ database/migrations/2021_11_16_180748_create_items_table.php
@@ -29,6 +29,9 @@ class CreateItemsTable extends Migration
      */
     public function down()
     {
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
         Schema::dropIfExists('items');
+        DB::statement('SET FOREIGN_KEY_CHECKS = 1');
+
     }
 }
